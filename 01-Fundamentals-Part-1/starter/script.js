a9df4710e79978c8e276e1ae56245dcd2e5f6470
Oren Nudelman <orennu@gmail.com>
2021-11-17 07:07:06 +0200

    se2-vd13 declaring variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
@@ -8,3 +9,38 @@ let firstName = "Liron";
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
+*/
+
+/*
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "oren");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1980;
+console.log(typeof year);
+
+let nul = null;
+console.log(nul);
+console.log(typeof nul);
+*/
+
+let age = 30;
+age = 31;
+
+console.log(age);
+
+const birthYear = 1980;
+// birthYear = 1990;
+
+const job;
