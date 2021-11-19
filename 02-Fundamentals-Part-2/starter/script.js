c8e0ddaf5b8b7fb05fa4118f10fa0c0598069154
Oren Nudelman <orennu@gmail.com>
2021-11-19 16:31:07 +0200

    se3-vd32 strict mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/*
+// strict mode
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true; // this will throw ReferenceError in console since hasDriverLicense is not defined (typo)
+if (hasDriversLicense) console.log('I can drive :D');
+
+const interface = 'audio'; // this will throw syntaxError in console due to usage of reserved word (interface) for variable name
+const private = 'Ryan'; // this is the same as above with different reserved word (private)
+*/
+
+
