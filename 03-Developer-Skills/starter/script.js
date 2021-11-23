9b8a1c8a2967357bd43b8f7d248923a9bbb22c0b
Oren Nudelman <orennu@gmail.com>
2021-11-24 00:16:10 +0200

    se5-vd58 problem solving

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,8 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = 23;
+const calcAge = birthYear => new Date().getFullYear() - birthYear;
+console.log(calcAge(1980));
+
+// TODO BUG
