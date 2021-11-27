a5e63f3d67b9beb2af2ad698b6da8b986c6e92f4
Oren Nudelman <orennu@gmail.com>
2021-11-27 21:53:15 +0200

    se7 guess my number fully functional after refactoring

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -25,6 +25,9 @@ document.querySelector('.check').addEventListener('click', function () {
 });
 */
 
+// guess my number game
+// before refactoring
+/*
 // generate a random number between 1-20
 const generateNumber = () => Math.trunc(Math.random() * 20) + 1;
 let secretNumber = generateNumber();
@@ -90,4 +93,81 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.guess').value = '';
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
+});
+*/
+
+// after refactoring
+// generate a random number between 1-20
+const generateNumber = () => Math.trunc(Math.random() * 20) + 1;
+let secretNumber = generateNumber();
+let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+const diplayScore = function (score) {
+  document.querySelector('.score').textContent = score;
+};
+
+const displayGuessNumber = function (num) {
+  document.querySelector('.number').textContent = num;
+};
+
+const setElementBackgroundColor = function (selector, color) {
+  document.querySelector(selector).style.backgroundColor = color;
+};
+
+const setElementWidth = function (selector, width) {
+  document.querySelector(selector).style.width = width;
+};
+
+// console.log(secretNumber);
+
+// event listener for click event on the check button
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  // when there is no input
+  if (!guess) {
+    displayMessage('⛔ No number!');
+
+    // when player wins (guess the correct number)
+  } else if (guess === secretNumber) {
+    displayGuessNumber(secretNumber);
+    displayMessage('🥳 Correct Number!');
+    setElementBackgroundColor('body', '#60b347');
+    setElementWidth('.number', '30rem');
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+
+    // when guess is different than secret number
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '⬆️ Too high!' : '⬇️ Too low!');
+      score--;
+      diplayScore(score);
+    } else {
+      displayMessage('🙁 You lost the game!');
+      setElementBackgroundColor('body', '#ff3838');
+      diplayScore(0);
+    }
+  }
+});
+
+// event listener for click event on the reset button
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = generateNumber();
+  //   console.log(secretNumber);
+  displayGuessNumber('?');
+  displayMessage('Start guessing...');
+  diplayScore(score);
+  document.querySelector('.guess').value = '';
+  setElementBackgroundColor('body', '#222');
+  setElementWidth('.number', '15rem');
 });
