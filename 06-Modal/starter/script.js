4f51c80eb4c0f8211b31d17b6cadc24d83b6e01c
Oren Nudelman <orennu@gmail.com>
2021-11-28 09:40:49 +0200

    se7 modal component

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,56 @@
 'use strict';
+
+/*
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+// selecting all elements with selector
+const btnsShowModal = document.querySelectorAll('.show-modal');
+
+// this will print the NodeList of matching elements
+console.log(btnsShowModal);
+
+// iterate over elements in NodeList
+for (let i = 0; i < btnsShowModal.length; i++) {
+  console.log(btnsShowModal[i].textContent);
+}
+*/
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+// adding global event listener since keyboard events are not specific to one element
+// if we want to see the event object passed to the handler function of the event listener, we can define a parameter and log it inside the handler. JavaScript will pass the event object as argument to our handler function
+/*
+document.addEventListener('keydown', function (event) {
+  console.log(event.key);
+
+  if (event.key === 'Escape') console.log('Esc was pressed');
+});
+*/
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
