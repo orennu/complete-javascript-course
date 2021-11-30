69e8c01bb74681c030ba013692c0824fa91308d0
Oren Nudelman <orennu@gmail.com>
2021-11-30 08:47:40 +0200

    se8-vd93 scoping

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -13,7 +13,32 @@ function calcAge(birthYear) {
     // output is defined in the printAge function scope, it cannot be accessed anywhere besides printAge scope
     // age and birthYear are found during variable lookup in the outer scope (calcAge function scope in this case)
     // firstName is also found during variable lookup in the outer scope, but in the global scope
-    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1980 && birthYear <= 1996) {
+      // millenial is considered a function scope variable, because it is defined with the var keyword, therefor it can be accessed from the function scope (printAge)
+      var millenial = true;
+      // it is possible to create new variable with the same name as out scope, the new variable is local to it's scope
+      const firstName = 'James';
+      // str is defined in the if scope with const, therefor it can only be accessed from this scope
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+      // functions in strict mode are blocked scope
+      function add(a, b) {
+        return a + b;
+      }
+
+      // it is possible to re-assign values to variables from outer scope, be carefull with this
+      output = 'NEW OUTPUT';
+    }
+    // str cannot be accessed from an outer scope and below line will raise ReferenceError
+    // console.log(str);
+    // millenial can be accessed from printAge function scope since it's a function scope variable
+    console.log(millenial);
+    // calling add in strict mode will raise a ReferenceError, if strict mode is disabled, add's scope becomes function scope and calling it from outside the if block and inside the printAge function scope will work
+    // console.log(add(2, 3));
+    // here we log the new output because we re-assigned it in the if block scope
     console.log(output);
   }
   // printAge can be called only from the scope where it's defined (calcAge function scope in this case)
