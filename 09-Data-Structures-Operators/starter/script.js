296584b27d51d53fd77899c838af0751b3cf3bdf
Oren Nudelman <orennu@gmail.com>
2021-12-08 00:00:19 +0200

    se9-vd103 destructuring arrays

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,5 +1,16 @@
 'use strict';
 
+// the naive way of unpacking array elements to seperate variables
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+// destructuring array
+const [x, y, z] = arr;
+
+console.log(x, y, z);
+
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
@@ -11,6 +22,10 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 
   openingHours: {
     thu: {
@@ -27,3 +42,41 @@ const restaurant = {
     },
   },
 };
+
+// take first 2 elements
+const [first, second] = restaurant.categories;
+
+console.log('take first 2 elements: ', first, second);
+
+// take 1sta and 3rd element
+const [el1, , el3] = restaurant.categories;
+
+console.log('take 1st and 3rd element: ', el1, el3);
+
+// switch variables
+let [main, , secondary] = restaurant.categories;
+
+console.log('before switch: ', main, secondary);
+
+[main, secondary] = [secondary, main];
+
+console.log('after switch: ', main, secondary);
+
+// receive 2 return values from a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+
+console.log('receive 2 return values from a function: ', starter, mainCourse);
+
+// nested array destructuring
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+
+// console.log(i, j);
+const [i, , [j, k]] = nested;
+
+console.log('nested destructuring: ', i, j, k);
+
+// set default for variables when extracting when we don't know the length of array
+const [p = 1, q = 1, r = 1] = [8, 9];
+
+console.log('destructuring with default values: ', p, q, r);
