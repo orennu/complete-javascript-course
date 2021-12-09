03210250350e7740e5056b3b95d5c1c2ace377c0
Oren Nudelman <orennu@gmail.com>
2021-12-09 23:59:05 +0200

    se9-vd108 nullish coalescing operator (??)

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -401,5 +401,62 @@ if (restaurant.orderPizza) {
 restaurant.orderPizza && restaurant.orderPizza('mushrum', 'spinach');
 */
 
+/*
 // the nullish coalescing operator (??)
 console.log('=== the nullish coalescing operator (??) ===');
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
+
+  orderPizza: function (mainIngredients, ...otherIngredients) {
+    console.log(mainIngredients);
+    console.log(otherIngredients);
+  },
+};
+
+// problem: 0 is falsy value so guests will be assigned to 10, our initial assumption was to assign 10 if the property is undefined
+restaurant.numGuests = 0;
+const guests = restaurant.numGuests || 10;
+console.log(guests); // 10
+
+// solution - nullish coalescing operator (??)
+const guestsCorrect = restaurant.numGuests ?? 10;
+console.log(guestsCorrect); // 0
+
+// nullish operator works with nullish values instead of falsy values
+// null
+// undefined
+// 0 and '' are treated as NON nullish
+
+const tables = restaurant.numTables ?? 20; // numTables is undefined
+console.log(tables); // 20
+*/
