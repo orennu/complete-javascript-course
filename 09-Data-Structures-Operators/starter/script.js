db1573f83989f056a9c227dd45e68fec6d38b80a
Oren Nudelman <orennu@gmail.com>
2021-12-10 16:33:28 +0200

    se9-vd109 logical assignment operators (||=/&&=/??=)

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -460,3 +460,60 @@ console.log(guestsCorrect); // 0
 const tables = restaurant.numTables ?? 20; // numTables is undefined
 console.log(tables); // 20
 */
+
+/*
+// logical assignment operators (||=/&&=/??=)
+console.log('=== logical assignment operators (||=/&&=) ===');
+
+const rest1 = {
+  name: 'Capri',
+  numGuests: 20,
+};
+
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossi',
+};
+
+const rest3 = {
+  name: 'Steak House',
+  numGuests: 0,
+  address: '21 Jump st.',
+};
+
+// OR assignment operator
+// pre ES 2021
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+rest1.numGuests ||= 10;
+rest2.numGuests ||= 10;
+rest3.numGuests ||= 10; // unexpected result 0 override by 10
+
+console.log(rest1); // 20
+console.log(rest2); // 10
+console.log(rest3); // 10
+
+
+// nullish assignment operator
+
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+rest3.numGuests ??= 10;
+
+console.log(rest1); // 20
+console.log(rest2); // 10
+console.log(rest3); // 0
+
+
+// AND assignment operator
+// pre ES 2021
+// rest1.owner = rest1.owner && '<ANONYMOUS>'; // owner: undefined
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+
+console.log(rest1); // no owner property
+console.log(rest2); // <ANONYMOUS>
+*/
